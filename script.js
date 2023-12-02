c071814da8e08efbe98329e9509e92fe682b4655
HELEN-DEE <helendeee12@gmail.com>
2023-12-03 00:15:48 +0100

    minor functionality

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
-const billInput = document.getElementById('bill').value;
-const peopleInput = document.getElementById('number').value;
+const billInput = document.getElementById('bill');
+const peopleInput = document.getElementById('number');
+const customInput = document.getElementById('custom');
+const tipAmount = document.getElementById('tip-amount')
+const splitBill = document.getElementById('split-bill')
 
-
+// const btnClick = document.getElementById('btn-click')
+const btnClick = document.querySelectorAll('.btn-click')
 const btnReset = document.getElementById('btnReset')
+
+btnClick.forEach(function(btnClick) {
+    btnClick.addEventListener('click', function() {
+        const btnInput = btnClick.textContent;
+        // console.log(btnInput)
+    });
+})
+
+const inputs = [billInput,peopleInput,customInput]
+
+inputs.forEach(function(input) {
+    input.addEventListener('input',function() {
+    const inputValue = input.value
+
+        console.log(inputValue)
+    })
+})
+
+btnReset.addEventListener('click', function() {
+    location.reload()
+})
+
+function calculateTip() {
+    const bill = parseFloat(billInput.value) || 0;
+    const noOfPeople = parseFloat(peopleInput.value) || 1;
+
+    let tipPercentage = 0
+    buttons.forEach(function(btnClick){
+        if(btnClick.classList.contains('active')){
+            
+        }
+    })
+}
