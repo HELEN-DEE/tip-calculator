3f3e9f0677c3b0f66f164dbd8bd1653f1f891f50
HELEN-DEE <helendeee12@gmail.com>
2023-12-08 20:08:39 +0100

    made corrections to the reset button

--- script.js
+++ script.js
@@ -16,6 +16,9 @@ const tipButtons = [
 const tipPerPersonOutput = document.getElementById('tip-per-person');
 const totalPerPersonOutput = document.getElementById('total-per-person');
 
+// Error message
+const errorMessage = document.getElementById('error-message');
+
 // Reset Button
 const btnReset = document.getElementById('btnReset');
 
@@ -29,13 +32,17 @@ tipButtons.forEach((button, index) => {
   button.addEventListener('click', function () {
     if (
       isNumeric(billTotalInput.value) &&
-      isNumeric(numberOfPeopleInput.value)
+      isNumeric(numberOfPeopleInput.value) &&
+      parseInt(numberOfPeopleInput.value) > 0
     ) {
+      resetError(); // Reset error message
       tipCalculator(
         parseFloat(billTotalInput.value),
         (index + 1) * 5,
         parseInt(numberOfPeopleInput.value)
       );
+    } else {
+      displayError("can't be 0");
     }
   });
 });
@@ -46,19 +53,32 @@ customPercentTip.addEventListener('input', function () {
   if (
     isNumeric(billTotalInput.value) &&
     isNumeric(numberOfPeopleInput.value) &&
-    !isNaN(customPercent)
+    !isNaN(customPercent) &&
+    parseInt(numberOfPeopleInput.value) > 0
   ) {
+    resetError(); // Reset error message
     tipCalculator(
       parseFloat(billTotalInput.value),
       customPercent,
       parseInt(numberOfPeopleInput.value)
     );
+  } else {
+    displayError('Number of people must be greater than 0.');
   }
 });
 
 btnReset.addEventListener('click', function () {
   resetFunction();
 });
+
+const resetFunction = () => {
+  resetError(); // Reset error message
+  billTotalInput.value = '';
+  numberOfPeopleInput.value = '';
+  customPercentTip.value = '';
+  tipPerPersonOutput.innerHTML = '$0.00';
+  totalPerPersonOutput.innerHTML = '$0.00';
+};
 
 // General Function: Calculate tip per person and total per person
 const tipCalculator = (billTotal, tipPercent, numberOfPeople) => {
@@ -71,17 +91,16 @@ const tipCalculator = (billTotal, tipPercent, numberOfPeople) => {
 
   tipPerPersonOutput.innerHTML = tipPerPerson.toFixed(2);
   totalPerPersonOutput.innerHTML = totalPerPerson.toFixed(2);
-
-  return {
-    tipPerPerson: tipPerPerson.toFixed(2),
-    totalPerPerson: totalPerPerson.toFixed(2),
-  };
 };
 
-const resetFunction = () => {
-  billTotalInput.value = '';
-  numberOfPeopleInput.value = '';
-  customPercentTip.value = '';
-  tipPerPersonOutput.innerHTML = '$0.00';
-  totalPerPersonOutput.innerHTML = '$0.00';
-};
+// Helper function to display error message
+function displayError(message) {
+  errorMessage.innerText = message;
+  errorMessage.style.display = 'block';
+}
+
+// Helper function to reset error message
+function resetError() {
+  errorMessage.innerText = '';
+  errorMessage.style.display = 'none';
+}
